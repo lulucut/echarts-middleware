658ed0d4477bccf54d40884c86fa559dc5bac446
蒲鸽 <my@owo.help>
2017-09-23 15:04:30 +0800

    start

---
+++ src/components/vue-echarts.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="echarts"></div>
+</template>
+
+<script>
+  import echarts from 'echarts/lib/echarts'
+  export default {
+    props: {
+      options: Object
+    },
+    data () {
+      return {
+        chart: null
+      }
+    },
+    mounted () {
+      if (this.options) {
+        let chart = echarts.init(this.$el, this.options)
+      }
+    }
+  }
+</script>
