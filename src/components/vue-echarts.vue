4cf427d4e9908fa7a35b3948c0f2eaf471ffaab0
蒲鸽 <my@owo.help>
2017-09-23 16:14:45 +0800

    save

--- src/components/vue-echarts.vue
+++ src/components/vue-echarts.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="echarts"></div>
+  <div class="echarts" :style="{ height: h + 'px', width: w +'px' }"></div>
 </template>
 
 <script>
-  import echarts from 'echarts/lib/echarts'
+  import echarts from 'echarts'
+  let myChart = null
   export default {
     props: {
-      options: Object
+      opt: Object,
+      h: {
+        type: Number,
+        default: 400,
+        validator: (value) => {
+          return value >= 0
+        }
+      },
+      w: {
+        type: Number,
+        default: 400,
+        validator: (value) => {
+          return value >= 0
+        }
+      }
     },
     data () {
       return {
@@ -14,9 +29,25 @@
       }
     },
     mounted () {
-      if (this.options) {
-        let chart = echarts.init(this.$el, this.options)
+      // 注册echarts
+      if (this.opt) {
+        let chart = null
+        chart = echarts.init(this.$el, this.opt)
+        // 绘制图表
+        chart.setOption(this.opt)
+        this.chart = chart
+        console.log(chart)
       }
+    },
+    beforeDestroy () {
+      // 释放echarts
+      this.chart.dispose()
     }
   }
 </script>
+
+<style>
+.echarts {
+  position: relative;
+}
+</style>
